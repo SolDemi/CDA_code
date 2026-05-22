959243a5b01ad3351b50f19916b533c26b28093a
SolDemi <h6706@outlook.com>
2026-05-22 18:15:11 +0800

    Include global alpha models in LDA decoding

--- LDA_decoding.m
+++ LDA_decoding.m
@@ -10,7 +10,7 @@ end
 
 outputdir = fullfile(statRoot, 'decoding_LDA_spatialControl', 'loadWithinSide');
 
-modelNames = {'CDA', 'Alpha', 'NoPCA', 'PCA'};
+modelNames = {'CDA', 'Alpha', 'GlobalAlpha', 'GlobalAlphaMean', 'NoPCA', 'PCA'};
 for i = 1:numel(modelNames)
     outFolder = fullfile(outputdir, modelNames{i});
     if ~isfolder(outFolder), mkdir(outFolder); end
