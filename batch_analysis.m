62c6598273f60e5c0292cbfdf015c8ef2f76220d
SolDemi <h6706@outlook.com>
2026-05-27 11:03:26 +0800

    Update batch_analysis.m

--- batch_analysis.m
+++ batch_analysis.m
@@ -11,66 +11,27 @@ cd(codeDir);
 addpath(codeDir);
 addpath(fullfile(codeDir, 'spatial_control_code'));
 
-cfg = struct();
-cfg.runData0LoadDecoding = true;
-cfg.runData0SpatialControl = true;
-cfg.runData1Preprocess = true;
-cfg.runData1CompareCDA = true;
-cfg.runData1SVM = true;
-cfg.runData1LDA = true;
-cfg.runGroupStats = true;
-cfg.runData3Preprocess = true;
+%% data0: 
+% load decoding
+process_data0
 
-fprintf('CDA batch analysis started.\nProject root: %s\n\n', projectRoot);
-
-%% data0: load decoding
-if cfg.runData0LoadDecoding
-    fprintf('Running data0 load decoding...\n');
-    run_script(fullfile(codeDir, 'process_data0.m'));
-end
-
-%% data0: spatial-control analyses
-if cfg.runData0SpatialControl
-    fprintf('Running data0 spatial-control decoding...\n');
-    run_script(fullfile(codeDir, 'spatial_control_code', 'process_spatial_control_decoding.m'));
-    run_script(fullfile(codeDir, 'spatial_control_code', 'stat_plot_spatial_control.m'));
-end
+% data0: spatial-control analyses
+process_spatial_control_decoding
+stat_plot
 
 %% data1: rebuild CDA/alpha and decode
-if cfg.runData1Preprocess
-    fprintf('Building data1 CDA/alpha files...\n');
-    run_script(fullfile(codeDir, 'cda_alpha.m'));
-end
+cda_alpha
 
-if cfg.runData1CompareCDA
-    fprintf('Comparing rebuilt CDA with source CDA...\n');
-    run_script(fullfile(codeDir, 'compareCDA.m'));
-end
+% Comparing rebuilt CDA with source CDA
+compareCDA
 
-if cfg.runData1SVM
-    fprintf('Running data1 SVM decoding...\n');
-    run_script(fullfile(codeDir, 'SVM_decoding.m'));
-    run_script(fullfile(codeDir, 'plot_SVM_result.m'));
-end
+SVM_decoding
+plot_SVM_result
 
-if cfg.runData1LDA
-    fprintf('Running data1 LDA decoding...\n');
-    run_script(fullfile(codeDir, 'LDA_decoding.m'));
-end
 
-if cfg.runGroupStats
-    fprintf('Running group-level decoding statistics...\n');
-    run_script(fullfile(codeDir, 'stat_plot.m'));
-end
+LDA_decoding
+
+stat_plot
 
 %% data3: build CDA/alpha files for the same decoding pipeline
-if cfg.runData3Preprocess
-    fprintf('Building data3 CDA/alpha files...\n');
-    run_script(fullfile(codeDir, 'data3_cda_alpha.m'));
-end
-
-fprintf('\nCDA batch analysis finished.\n');
-
-function run_script(scriptPath)
-    run(scriptPath);
-end
+data3_cda_alpha
